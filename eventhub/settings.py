8f75ce9e6957c4f5986380a9d2d179a63883e8cb
ninjahurricane007 <67139570+ninjahurricane007@users.noreply.github.com>
2022-11-11 12:36:06 +0530

    Update settings.py

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-13b8q1eqrd&amqhxw1$v&olg&2qm*!=c(btq55r%+_kdt_88rm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
